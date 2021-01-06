53901a7d13640010e21ad1fcb8be41ea2cb6f7fe
Chris McGee <chris@chrismcgee.info>
2021-01-06 10:48:53 -0500

    🚧 Initial IIFE build

--- dist/half-retina.js
+++ dist/half-retina.js
@@ -1,5 +1,26 @@
 "use strict";
 (function () {
-    var message = "Hello World";
-    $.writeln(message);
+    var wInput = new Window("dialog", "Half-Retina");
+    wInput.alignChildren = "right";
+    var sNote = wInput.add("statictext", undefined, "Note: case-sensitive.");
+    sNote.alignment = "center";
+    var gInput = wInput.add("group");
+    gInput.add("statictext", undefined, "Directory:");
+    var tInput = gInput.add("edittext");
+    var bBrowse = gInput.add("button");
+    tInput.characters = 20;
+    tInput.active = true;
+    var gButtons = wInput.add("group");
+    var bOK = gButtons.add("button", undefined, "Begin", { name: "ok" });
+    var bCancel = gButtons.add("button", undefined, "Cancel");
+    bOK.enabled = false;
+    tInput.onChanging = function () {
+        bOK.enabled = !!tInput.text;
+    };
+    bBrowse.onClick = function () {
+        tInput.text;
+    };
+    if (wInput.show() === 1) {
+        $.writeln("Test");
+    }
 })();
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,46 @@
 /// <reference types="types-for-adobe/Photoshop/2015.5"/>
 
 (() => {
-  const message: string = `Hello World`;
-  $.writeln(message);
+  // Build ScriptUI window
+  const wInput: Window = new Window(`dialog`, `Half-Retina`);
+  wInput.alignChildren = `right`;
+
+  const sNote: StaticText = wInput.add(
+    `statictext`,
+    undefined,
+    `Note: case-sensitive.`
+  );
+  sNote.alignment = `center`;
+
+  const gInput: Group = wInput.add(`group`);
+  gInput.add(`statictext`, undefined, `Directory:`);
+  const tInput: EditText = gInput.add(`edittext`);
+  const bBrowse: Button = gInput.add(`button`);
+  tInput.characters = 20;
+  tInput.active = true;
+
+  const gButtons: Group = wInput.add(`group`);
+  const bOK: Button = gButtons.add(`button`, undefined, `Begin`, {name: `ok`});
+  // eslint-disable-next-line no-unused-vars
+  const bCancel: Button = gButtons.add(`button`, undefined, `Cancel`);
+
+  bOK.enabled = false;
+  tInput.onChanging = () => {
+    bOK.enabled = !!tInput.text;
+  };
+
+  // Define the behavior of the "Browse" button
+  bBrowse.onClick = () => {
+    tInput.text;
+  };
+
+  if (wInput.show() === 1) {
+    // const find: string = tInput.text;
+    // const replaceWith: string = tOutput.text;
+
+    // const rootLayers: Layers = workDoc.layers;
+
+    // renameLayer(rootLayers, find, replaceWith);
+    $.writeln("Test");
+  }
 })();
